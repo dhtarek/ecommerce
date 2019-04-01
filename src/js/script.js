082ade647154c07578b809d48726c393ee7590de
kouskous Tarek <dhtarek@gmail.com>
2019-04-01 06:40:19 +0200

    finish second carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
+    $('[data-add-to-cart]').click(function(e){
+        alert('أضيف العنصر إلى عربة الشراء');
+        e.stopPropagation();
+    });
+    
 });
+
+
